bfe75940799876d3b3884220c8cfe680931d8bd1
Kest <meowtss@protonmail.com>
2025-02-05 00:45:26 -0500

    Added source files

---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    handleLogin() {
+      axios.post('/api/login', {
+        email: this.$refs.email.value,
+        password: this.$refs.password.value
+      })
+      .then((response) => {
+        console.log("Found user")
+      })
+      .catch((error) => {
+        console.log("failed")
+      })
+    }
+  }
+}
+</script>
+
+<template>
+  <main>
+    <input type="text" ref="email">
+    <input type="text" ref="password">
+    <button @click="handleLogin">Login</button>
+  </main>
+</template>
